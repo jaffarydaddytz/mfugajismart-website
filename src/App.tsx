c770dac25b5be29d678fc4abcb3ca370d3f357bd
jaffarydaddy <jaffarydaddytz@gmail.com>
2025-02-19 08:54:44 +0300

    add language switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,65 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import i18n from 'i18next'
+import { Box, Button, Container, Typography } from '@mui/material'
+import { blue } from '@mui/material/colors'
 
+function App () {
+  const { t }: { t: (key: string) => string } = useTranslation()
 
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng)
+  }
 
-function App() {
-  const { t }: { t: (key: string) => string } = useTranslation();
-  
   return (
-    <div >
-      <h1>{t('greeting')}</h1>
+    <div>
+      <Container
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          gap: 1,
+          flexDirection: 'column'
+        }}
+      >
+        <Box>
+          {' '}
+          <Typography
+            variant='h1'
+            sx={{ fontSize: '32px', fontWeight: 'bold' }}
+          >
+            {' '}
+            {t('greeting')}{' '}
+          </Typography>
+        </Box>
 
+        <Box sx={{backgroundColor:blue[50], padding:2, borderRadius:'5px'}}>
+          <Typography variant='h3' sx={{ fontSize: '24px' }}>
+            {' '}
+            {t('herocontent')}{' '}
+          </Typography>
+        </Box>
+
+        <Box>
+          <Button
+            variant='contained'
+            sx={{ marginX: 1 }}
+            onClick={() => changeLanguage('sw')}
+          >
+            Swahili
+          </Button>
+          <Button
+            variant='contained'
+            sx={{ marginX: 1 }}
+            onClick={() => changeLanguage('en')}
+          >
+            English
+          </Button>
+        </Box>
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,30 +1,19 @@
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
+import Backend from 'i18next-http-backend'
 
-i18n.use(LanguageDetector).use(initReactI18next).init({
+i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
     debug:true,
     lng:'en',
-    resources:{
-
-        en:{
-
-            translation:{
-                greeting:'Hello, welcome to Mfugajismart',
-
-            },
-
-        },
-        sw:{
-
-            translation:{
-                greeting:'Habari, Karibu katika Mfugajismart',
-
-            },
-
-        }
-
+    backend:{
+        loadPath:'/locales/{{lng}}/translation.json',
+    },
+    react:{
+        useSuspense:false,
     }
+
+    
 })
 
 export default i18n;
